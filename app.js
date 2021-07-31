5b25dc9b023fc04d8b781f1740ff4f6fa6cd42d1
tmem11 <tmem.nsasra@gmail.com>
2021-07-31 23:18:22 +0300

    add simple video maker that usees the taken screenshots

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 const screenShotTaker=require('./takeScreenShot');
-screenShotTaker.takeScreenShot('https://google.com/');
+var videoshow = require('videoshow')
+//screenShotTaker.takeScreenShot('https://google.com/');
+var images = [
+     'screenshot.png'
+]
+var videoOptions = {
+    fps: 25,
+    loop: 10, // seconds
+    transition: true,
+    transitionDuration: 1, // seconds
+    videoBitrate: 1024,
+    videoCodec: 'libx264',
+    size: '640x?',
+    audioBitrate: '128k',
+    audioChannels: 2,
+    format: 'mp4',
+    pixelFormat: 'yuv420p'
+}
+
+videoshow(images)
+    .save('video.mp4')
+    .on('start', function (command) {
+        console.log('ffmpeg process started:', command)
+    })
+    .on('error', function (err) {
+        console.error('Error:', err)
+    })
+    .on('end', function (output) {
+        console.log('Video created in:', output)
+    })
+
